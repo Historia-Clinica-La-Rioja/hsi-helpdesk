57799da0adc7a17c4c09ae913f7b822eefeb6e3e
RodrigoCba96 <rodrigocordoba2296@gmail.com>
2026-07-03 09:49:19 -0300

    advaces with chatbot static and faq conexion

--- backend/internal/models/faq.go
+++ backend/internal/models/faq.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Faq struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Label     string             `bson:"Label" json:"label"`
+	Questions string             `bson:"Questions" json:"questions"`
+	Answers   string             `bson:"Answers" json:"answers"`
+	IsActive  bool               `bson:"isActive" json:"is_active"`
+	CreatedOn time.Time          `bson:"created_on" json:"created_on"`
+	UpdatedOn time.Time          `bson:"updated_on" json:"updated_on"`
+}
